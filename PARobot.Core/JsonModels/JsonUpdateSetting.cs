f2316492b407ed6bd3074d3231739b6b2fe1b4b7
tonyjt <tonyliujiangtao@gmail.com>
2012-10-10 00:35:52 +0800

    1.11

---
+++ PARobot.Core/JsonModels/JsonUpdateSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PARobot.Core.JsonModels
+{
+    public class JsonUpdateSetting
+    {
+        public int MaxSettingVersion { get; set; }
+    }
+}
